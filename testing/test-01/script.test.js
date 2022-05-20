4ae4f2f44eeccfadd453f6a9d2c6804a1f1975e0
wakidurrahman <wakidur.rahman@bjitgroup.com>
2022-05-20 17:17:58 +0600

    writing tests

--- testing/test-01/script.test.js
+++ testing/test-01/script.test.js
@@ -13,10 +13,33 @@ dbMock = [
   "mail.ru",
 ];
 
-it("This is a test", () => {
-  expect("hello").toBe("hello");
+describe("searchingString", () => {
+  it("this is a silly test should be say Hello", () => {
+    expect("Hello").toBe("Hello");
+  });
+
+  it("This is a searchingString function", () => {
+    expect(searchingString(dbMock, "test")).toStrictEqual([]);
+    expect(searchingString(dbMock, "com")).toStrictEqual([
+      "bp.blogspot.com",
+      "feedburner.com",
+      "vimeo.com",
+    ]);
+    expect(searchingString(dbMock, "bp")).toEqual(["bp.blogspot.com"]);
+  });
+
+  it("Work with undefined and null input", () => {
+    expect(searchingString(dbMock, undefined)).toEqual([]);
+    expect(searchingString(dbMock, null)).toEqual([]);
+  });
+
+  it("does not return more than 3 matchs", () => {
+    expect(searchingString(dbMock, ".com").length).toEqual(3);
+  });
 });
 
-it("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("sum", () => {
+  it("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 });
