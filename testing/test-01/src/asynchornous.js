dcff6ee2b0aebcc789a9c0eb081b4029f285f0df
wakidurrahman <wakidur.rahman@bjitgroup.com>
2022-05-24 09:02:48 +0600

    all essential

--- testing/test-01/src/asynchornous.js
+++ testing/test-01/src/asynchornous.js
@@ -13,7 +13,6 @@ const getPeoplePromise = (fetch) => {
 const getPeople = async (fetch) => {
   const getRequest = await fetch("http://swapi.py4e.com/api/people");
   const data = await getRequest.json();
-  // console.log(data);
   return {
     count: data.count,
     results: data.results,
